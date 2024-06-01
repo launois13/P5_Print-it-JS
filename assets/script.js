63f2135fd82446816dcc5a08da42b5f129970eda
launois13 <pascalelaunois@free.fr>
2024-06-02 01:12:45 +0200

    commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Définition des slides
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,6 +18,7 @@ const slides = [
 	}
 ];
 
+//Sélection des éléments du DOM
 const bannerImg = document.querySelector('.banner-img');
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
@@ -24,34 +26,35 @@ const dots = document.querySelectorAll('.dot'); // Sélectionne tous les points
 
 let currentIndex = 0;
 
-// Mise à jour les points indicateurs "dots"
+// Mise à jour des "dots"
 function updateDots(index) {
     dots.forEach((dot, i) => {
         if (i === index) {
             dot.classList.add('dot_selected'); // Ajoute la classe pour le point actuel
         } else {
-            dot.classList.remove('dot_selected'); // Supprime la classe pour les autres points
+            dot.classList.remove('dot_selected'); // Suppression de la classe pour les autres points
         }
     });
 }
 
-// Mise à jour des "dots", images et textes
+// Mise à jour du carrousel
 function updateCarousel(index, direction) {
+	// Gestion des index pour le défilement infini
         if (currentIndex === -1 && direction === 'left') {
         currentIndex = slides.length - 1;
     	} else if (currentIndex === slides.length && direction === 'right') {
         currentIndex = 0;
     }
 
-    //Mise à jour image
+    // Mise à jour image et texte
     const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
     bannerImg.src = imagePath;
     bannerImg.alt = `Slide ${currentIndex + 1}`;
 
-    // Mise à jour texte
     const tagLine = slides[currentIndex].tagLine;
     document.querySelector('p').innerHTML = tagLine;
 
+    // Affichage dans la console du navigateur pour le débogage
     console.log(`Clic sur la flèche ${direction}`);
 }
 
@@ -70,6 +73,6 @@ arrowRight.addEventListener('click', function () {
 });
 
 
-// Afficher la première diapositive au chargement de la page
+// Affiche la première diapositive au chargement de la page
 updateCarousel(currentIndex, 'démarrage');
 updateDots(currentIndex); // Mise à jour des "dots" pour la première diapositive
