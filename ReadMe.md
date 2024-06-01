734afd3622b0c7033d6f27422ded099797574161
launois13 <pascalelaunois@free.fr>
2024-06-01 20:03:02 +0200

    js ok

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
-# Print-It WebSite v.1
+# Projet 5 : Print-it
+Intégration d'un carrousel dynamique 
+html   /   Css   /   JavaScript
--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,61 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+const bannerImg = document.querySelector('.banner-img');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot'); // Sélectionne tous les points
+
+let currentIndex = 0;
+
+// Mise à jour les points indicateurs "dots"
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('dot_selected'); // Ajoute la classe pour le point actuel
+        } else {
+            dot.classList.remove('dot_selected'); // Supprime la classe pour les autres points
+        }
+    });
+}
+
+// Mise à jour des points indicateurs, images et textes
+function updateCarousel(index, direction) {
+        if (currentIndex === -1 && direction === 'left') {
+        currentIndex = slides.length - 1;
+    	} else if (currentIndex === slides.length && direction === 'right') {
+        currentIndex = 0;
+    }
+
+    // Mettre à jour l'image
+    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.src = imagePath;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+    // Mettre à jour le texte
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+
+    console.log(`Clic sur la flèche ${direction}`);
+}
+
+// Gestionnaire d'événement pour le clic sur la flèche gauche
+arrowLeft.addEventListener('click', function () {
+    currentIndex = (currentIndex - 1);
+    updateCarousel(currentIndex, 'left');
+    updateDots(currentIndex); // Mise à jour des points indicateurs
+});
+
+// Gestionnaire d'événement pour le clic sur la flèche droite
+arrowRight.addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) ;
+    updateCarousel(currentIndex, 'right');
+    updateDots(currentIndex); // Mettez à jour les points indicateurs
+});
+
+
+// Afficher la première diapositive au chargement de la page
+updateCarousel(currentIndex, 'démarrage');
+updateDots(currentIndex); // Mise à jour des points indicateurs pour la première diapositive
